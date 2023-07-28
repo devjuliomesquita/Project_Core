641c64f93636a428671c91afd00ebd172293cebe
Júlio César Mesquita Camilo Martins <juliocesarmcamilo@gmail.com>
2023-07-28 15:43:55 -0300

    Inital Commit

---
+++ src/test/java/com/cleanarquiteture/projectcore/ProjectcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cleanarquiteture.projectcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectcoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
